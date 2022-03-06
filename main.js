675321e38150648f545af2687a42c81ba75ec6aa
VARUN MODI <SHaDoW8386>
2022-03-06 13:04:44 +0530

    data connected

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 var search_city = ["Mandla", "Mandsaur", "Indore", "Dewas", "Ujjain", "Jabalpur", "Shimla", "Manipur", "Khandwa", "Khargone", "Mumbai", "Delhi","Udaipur"];
 var search_state = ["Madhya Pradesh", "Bihar", "Punjab", "Rajasthan", "Maharastra", "Uttar Pradesh", "Gujarat"]
 var search_crop = ["Soybean", "Rice", "Bean", "Peas", "Gram", 'Wheat']
+var state = document.getElementById("state")
+var city = document.getElementById("city")
+var crop = document.getElementById("crop")
 
+function myCharData(){
+  var cdata =  fetch('./data.json')
+  return cdata
+  }
 
 
 function autocompleteMatch(input, q) {
@@ -41,36 +48,85 @@ function showQuery(val, q) {
   }
   res.innerHTML = '';
   let list = '';
-  // console.log(val)
   var terms = autocompleteMatch(val, q)
-  // console.log(terms)
   for (i=0; i<terms.length; i++) {
-    // console.log(list)
     list += '<button class="button-option" id="'+q+''+i+'" onclick="send(this.id, '+q+')">' + terms[i] + '</button>';
   }
   res.innerHTML = list;
-  // console.log(q)
 }
 
 function send(atr, q){
   var attr = document.getElementById(atr)
   q.value = attr.innerText;
-  // console.log( atr, q)  
 }
 
-// document.querySelector('#form1').addEventListener("submit", function(event){
-//     event.preventDefault();
-// });
 
-// document.querySelector('#form2').addEventListener("submit", function(event){
-//   event.preventDefault();
-// });
-// document.querySelector('#form3').addEventListener("submit", function(event){
-//   event.preventDefault();
-// });
-// document.querySelector('#form4').addEventListener("submit", function(event){
-//   event.preventDefault();
-// });
+//---------------------------------------------FIRST FORM SUBMIT------------------------------------------------------------------
+
+document.querySelector('#form1').addEventListener("submit", function(event){
+    event.preventDefault();
+    var select_state = window.state.value
+    var select_city = window.city.value
+    var select_crop = window.crop.value
+    window.search_city = []
+    window.search_crop = []
+    var data = myCharData()
+    data.then(response => response.json())
+    .then((data) => {
+      console.log(Object.keys(data[select_state]))
+      select_city = Object.keys(data[select_state])[0]
+      select_crop = Object.keys(data[select_state][select_city])[0]
+      window.city.value=select_city
+      window.crop.value=select_crop
+      window.search_city = Object.keys(data[select_state])
+      window.search_crop = Object.keys(data[select_state][select_city])
+      showQuery("","city");
+      showQuery("","state");
+      showQuery("","crop");
+      graph(select_state, select_city, select_crop);
+      myFunction(1)
+    })
+});
+
+
+//-----------------------------------------SECOND FORM SUBMIT-------------------------------------------------------
+
+document.querySelector('#form2').addEventListener("submit", function(event){
+    event.preventDefault();
+    var select_state = window.state.value
+    var select_city = window.city.value
+    var select_crop = window.crop.value
+    window.search_crop = []
+    var data = myCharData()
+    data.then(response => response.json())
+    .then((data) => {
+        for(i in data){
+          for(j in data[i]){
+            if(j==city){
+              select_state = i;
+              break;
+            }
+          }
+        }
+      window.search_crop = Object.keys(data[select_state][select_city])
+      showQuery("","city");
+      showQuery("","state");
+      showQuery("","crop");
+      graph(select_state, select_city, select_crop);
+      myFunction(2)
+    })
+    
+});
+
+
+//---------------------------------THIRD FORM SUBMIT----------------------------------------------------
+document.querySelector('#form3').addEventListener("submit", function(event){
+  event.preventDefault();
+  myFunction(3)
+});
+document.querySelector('#form4').addEventListener("submit", function(event){
+  event.preventDefault();
+});
 
 
 function myFunction(n) {
@@ -99,70 +155,69 @@ function myFunction(n) {
 
 //********************** For Graph *****************************************
 
-let cha = [];
 
-function myCharData(){
-var cdata =  fetch('./data.json')
-return cdata
 
+function graph(state, city, crop){
+  var data = myCharData()
+  data.then(response => response.json())
+  .then((data) => {
+    var min = []
+    var max = []
+    var avg = []
+    var x_axis = []
+    var select_graph = data[state][city][crop]
+
+    for(i in select_graph){
+      console.log(Object.keys(select_graph[i]))
+      max.push(select_graph[i].max)
+      min.push(select_graph[i].min)
+      avg.push(select_graph[i].avg)
+      x_axis.push(select_graph[i].date)
+    }
+
+    console.log(max)
+    console.log(x_axis)
+
+  var Min = {
+    x: x_axis,
+    y: min,
+    type: 'scatter',
+    line: {shape: 'spline',width:5, color:'blue'},
+    marker: {
+      color: 'black',
+      size: 15
+    },
+  };
+
+  var Max = {
+    x: x_axis,
+    y: max,
+    type: 'scatter',
+    line: {shape: 'spline',width:5, color:'red'},
+    marker: {
+      color: 'black',
+      size: 15
+    },
+  };
+
+  var Avg = {
+    x: x_axis,
+    y: avg,
+    type: 'scatter',
+    line: {shape: 'spline',width:5, color:'green'},
+    marker: {
+      color: 'black',
+      size: 15
+    },
+    
+  };
+
+  var layout = {
+    height: 650
+  };
+
+  var datum = [Min, Max, Avg];
+
+  Plotly.newPlot('myDiv', datum, layout);
+})
 }
-
-var character = []
-var data = myCharData()
-data.then(response => response.json())
-.then((data) => {
-  cha.push(data['Madhya Pradesh'])
-  // cha = data;
-} )
-
-
-// console.log(d)
-console.log(cha)
-
-
-
-
-
-
-var Min = {
-  x: [20, 21, 22, 23, 24, 25],
-  y: [1500, 1550, 1370, 1780, 1580, 1600],
-  type: 'scatter',
-  line: {shape: 'spline',width:5, color:'blue'},
-  marker: {
-    color: 'black',
-    size: 15
-  },
-};
-
-var Max = {
-  x: [20, 21, 22, 23, 24, 25],
-  y: [2160, 2250, 2110, 1920, 2000, 2060],
-  type: 'scatter',
-  line: {shape: 'spline',width:5, color:'red'},
-  marker: {
-    color: 'black',
-    size: 15
-  },
-};
-
-var Avg = {
-  x: [20, 21, 22, 23, 24, 25],
-  y: [1700, 1850, 1690, 1870, 1800, 1750],
-  type: 'scatter',
-  line: {shape: 'spline',width:5, color:'green'},
-  marker: {
-    color: 'black',
-    size: 15
-  },
-  
-};
-
-var layout = {
-  // width: 500,
-  height: 650
-};
-
-var datum = [Min, Max, Avg];
-
-Plotly.newPlot('myDiv', datum, layout);
